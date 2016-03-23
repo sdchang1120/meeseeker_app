37ac6332965807adfcda3b23449a04f890a8a48d
Sharon Chang <changsharond@gmail.com>
2016-03-23 12:56:44 -0400

    adding comments in meeseek model

--- app/models/meeseek.rb
+++ app/models/meeseek.rb
@@ -1,4 +1,6 @@
 class Meeseek < ActiveRecord::Base
+  # specifying the attribute we want Geocoder to convert
   geocoded_by :address
+  # sends request to external API (default: Google Maps API) if address field changes
   after_validation :geocode, :if => :address_changed?
 end
