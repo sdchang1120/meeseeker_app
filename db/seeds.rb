10965abe28cca8b078ed5f832a38a957e6297cf5
Sharon Chang <changsharond@gmail.com>
2016-03-23 13:16:25 -0400

    seeding and adjusting meeseeks index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Meeseek.create([
+  {
+    name: 'Sharon',
+    address: '7 Pebble Lane, Roslyn Heights, NY 11577',
+    latitude: 40.776378,
+    longitude: -73.630226
+  }, {
+    name: 'Meh',
+    address: '10 E 21st Street, New York, NY 10010',
+    latitude: 40.740112,
+    longitude: -73.990071
+  }
+])
